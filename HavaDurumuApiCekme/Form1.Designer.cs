a5ca844ceb5f9f075d4c47bbef128957fb6c4e0b
mahmut <st_falcon@outlook.com>
2023-02-06 11:17:22 +0300

    Proje dosyası ekle.

---
+++ HavaDurumuApiCekme/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace HavaDurumuApiCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelSehir = new System.Windows.Forms.Label();
+            this.labelHavaDurumu = new System.Windows.Forms.Label();
+            this.labelSicaklik = new System.Windows.Forms.Label();
+            this.labelBulutDurumu = new System.Windows.Forms.Label();
+            this.labelHissedilenSicaklik = new System.Windows.Forms.Label();
+            this.labelNem = new System.Windows.Forms.Label();
+            this.labelBasınç = new System.Windows.Forms.Label();
+            this.labelGunDogumu = new System.Windows.Forms.Label();
+            this.labelRuzgarHizi = new System.Windows.Forms.Label();
+            this.labelGunBatimi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(128, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 28);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // labelSehir
+            // 
+            this.labelSehir.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelSehir.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSehir.Location = new System.Drawing.Point(25, 83);
+            this.labelSehir.Name = "labelSehir";
+            this.labelSehir.Size = new System.Drawing.Size(198, 48);
+            this.labelSehir.TabIndex = 0;
+            this.labelSehir.Text = "Şehir:";
+            this.labelSehir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelSehir.Click += new System.EventHandler(this.labelSehir_Click);
+            // 
+            // labelHavaDurumu
+            // 
+            this.labelHavaDurumu.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelHavaDurumu.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHavaDurumu.Location = new System.Drawing.Point(25, 140);
+            this.labelHavaDurumu.Name = "labelHavaDurumu";
+            this.labelHavaDurumu.Size = new System.Drawing.Size(448, 43);
+            this.labelHavaDurumu.TabIndex = 1;
+            this.labelHavaDurumu.Text = "Hava Durumu:";
+            this.labelHavaDurumu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelSicaklik
+            // 
+            this.labelSicaklik.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelSicaklik.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSicaklik.Location = new System.Drawing.Point(25, 192);
+            this.labelSicaklik.Name = "labelSicaklik";
+            this.labelSicaklik.Size = new System.Drawing.Size(202, 49);
+            this.labelSicaklik.TabIndex = 2;
+            this.labelSicaklik.Text = "Sıcaklık:";
+            this.labelSicaklik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelBulutDurumu
+            // 
+            this.labelBulutDurumu.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBulutDurumu.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelBulutDurumu.Location = new System.Drawing.Point(25, 258);
+            this.labelBulutDurumu.Name = "labelBulutDurumu";
+            this.labelBulutDurumu.Size = new System.Drawing.Size(448, 41);
+            this.labelBulutDurumu.TabIndex = 3;
+            this.labelBulutDurumu.Text = "Bulut Durumu:";
+            this.labelBulutDurumu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelHissedilenSicaklik
+            // 
+            this.labelHissedilenSicaklik.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelHissedilenSicaklik.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHissedilenSicaklik.Location = new System.Drawing.Point(255, 192);
+            this.labelHissedilenSicaklik.Name = "labelHissedilenSicaklik";
+            this.labelHissedilenSicaklik.Size = new System.Drawing.Size(218, 46);
+            this.labelHissedilenSicaklik.TabIndex = 4;
+            this.labelHissedilenSicaklik.Text = "Hissedilen Sıcaklık";
+            this.labelHissedilenSicaklik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelNem
+            // 
+            this.labelNem.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelNem.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelNem.Location = new System.Drawing.Point(255, 83);
+            this.labelNem.Name = "labelNem";
+            this.labelNem.Size = new System.Drawing.Size(218, 46);
+            this.labelNem.TabIndex = 5;
+            this.labelNem.Text = "Nem:";
+            this.labelNem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelNem.Click += new System.EventHandler(this.labelNem_Click);
+            // 
+            // labelBasınç
+            // 
+            this.labelBasınç.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBasınç.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelBasınç.Location = new System.Drawing.Point(255, 311);
+            this.labelBasınç.Name = "labelBasınç";
+            this.labelBasınç.Size = new System.Drawing.Size(218, 46);
+            this.labelBasınç.TabIndex = 6;
+            this.labelBasınç.Text = "Basınç:";
+            this.labelBasınç.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelGunDogumu
+            // 
+            this.labelGunDogumu.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelGunDogumu.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelGunDogumu.Location = new System.Drawing.Point(25, 367);
+            this.labelGunDogumu.Name = "labelGunDogumu";
+            this.labelGunDogumu.Size = new System.Drawing.Size(202, 48);
+            this.labelGunDogumu.TabIndex = 7;
+            this.labelGunDogumu.Text = "Gün Doğumu:";
+            this.labelGunDogumu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelRuzgarHizi
+            // 
+            this.labelRuzgarHizi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRuzgarHizi.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelRuzgarHizi.Location = new System.Drawing.Point(25, 311);
+            this.labelRuzgarHizi.Name = "labelRuzgarHizi";
+            this.labelRuzgarHizi.Size = new System.Drawing.Size(202, 46);
+            this.labelRuzgarHizi.TabIndex = 8;
+            this.labelRuzgarHizi.Text = "Rüzgar Hızı:";
+            this.labelRuzgarHizi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelRuzgarHizi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelGunBatimi
+            // 
+            this.labelGunBatimi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelGunBatimi.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelGunBatimi.Location = new System.Drawing.Point(255, 367);
+            this.labelGunBatimi.Name = "labelGunBatimi";
+            this.labelGunBatimi.Size = new System.Drawing.Size(218, 48);
+            this.labelGunBatimi.TabIndex = 9;
+            this.labelGunBatimi.Text = "Gün Batımı:";
+            this.labelGunBatimi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(21, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Şehir Giriniz: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 459);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelGunBatimi);
+            this.Controls.Add(this.labelRuzgarHizi);
+            this.Controls.Add(this.labelGunDogumu);
+            this.Controls.Add(this.labelBasınç);
+            this.Controls.Add(this.labelNem);
+            this.Controls.Add(this.labelHissedilenSicaklik);
+            this.Controls.Add(this.labelBulutDurumu);
+            this.Controls.Add(this.labelSicaklik);
+            this.Controls.Add(this.labelHavaDurumu);
+            this.Controls.Add(this.labelSehir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelSehir;
+        private System.Windows.Forms.Label labelHavaDurumu;
+        private System.Windows.Forms.Label labelSicaklik;
+        private System.Windows.Forms.Label labelBulutDurumu;
+        private System.Windows.Forms.Label labelHissedilenSicaklik;
+        private System.Windows.Forms.Label labelNem;
+        private System.Windows.Forms.Label labelBasınç;
+        private System.Windows.Forms.Label labelGunDogumu;
+        private System.Windows.Forms.Label labelRuzgarHizi;
+        private System.Windows.Forms.Label labelGunBatimi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HavaDurumuApiCekme/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace HavaDurumuApiCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public void havaDurumuAl(string sehirr)
+        {
+            string api = "231a1d38680883db83264b3d4c06178b";
+            string bağlantı = $"https://api.openweathermap.org/data/2.5/weather?q={sehirr}&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument hava = XDocument.Load("https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api);
+            bool kontrol = true;
+            try
+            {
+                 hava = XDocument.Load(bağlantı);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Bir Şehir İsmi Giriniz");
+                kontrol = false;
+            }
+            
+            
+            
+            var hissedilensicaklik = hava.Descendants("feels_like").ElementAt(0).Attribute("value").Value;
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var ruzgarhizi = hava.Descendants("speed").ElementAt(0).Attribute("value").Value + hava.Descendants("speed").ElementAt(0).Attribute("unit").Value;
+            var havadurumu = hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            string sehir = hava.Descendants("city").ElementAt(0).Attribute("name").Value;
+            var gundogumu = hava.Descendants("sun").ElementAt(0).Attribute("rise").Value;
+            var gunbatimi = hava.Descendants("sun").ElementAt(0).Attribute("set").Value;
+            var bulutdurumu = hava.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+            var nem = hava.Descendants("humidity").ElementAt(0).Attribute("unit").Value + hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+            var basinc = hava.Descendants("pressure").ElementAt(0).Attribute("value").Value + hava.Descendants("pressure").ElementAt(0).Attribute("unit").Value;
+
+            gundogumu = gundogumu.Substring(11, 8);
+            gunbatimi = gunbatimi.Substring(11, 8);
+           
+
+            int artirilankisim = Convert.ToInt32(gundogumu.Substring(0, 2));
+            if (artirilankisim<21)
+            {
+
+                string[] timeParts = gundogumu.Split(':');
+                int hour = Convert.ToInt32(timeParts[0]);
+                hour += 3;
+                gundogumu = hour.ToString() + ":" + timeParts[1] + ":" + timeParts[2];
+            }
+            else
+            {
+                switch (artirilankisim)
+                {
+                    case 21:
+                        gundogumu.Replace("21", "0");
+                        break;
+                    case 22:
+                        gundogumu.Replace("22", "1");
+                        break;
+                    case 23:
+                        gundogumu.Replace("23", "2");
+                        break;
+                    case 24:
+                        gundogumu.Replace("24", "3");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            artirilankisim = Convert.ToInt32(gunbatimi.Substring(0, 2));
+            if (artirilankisim < 21)
+            {
+
+                string[] timeParts = gunbatimi.Split(':');
+                int hour = Convert.ToInt32(timeParts[0]);
+                hour += 3;
+                gunbatimi = hour.ToString() + ":" + timeParts[1] + ":" + timeParts[2];
+            }
+            else
+            {
+                switch (artirilankisim)
+                {
+                    case 21:
+                        gunbatimi.Replace("21", "0");
+                        break;
+                    case 22:
+                        gunbatimi.Replace("22", "1");
+                        break;
+                    case 23:
+                        gunbatimi.Replace("23", "2");
+                        break;
+                    case 24:
+                        gunbatimi.Replace("24", "3");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (kontrol==true)
+            {
+                labelSicaklik.Text = "Sıcaklık: " + sicaklik + "°C";
+                labelHissedilenSicaklik.Text = "Hissedilen Sıcaklık: " + hissedilensicaklik + "°C";
+                labelHavaDurumu.Text = "Hava Durumu: " + havadurumu;
+                labelRuzgarHizi.Text = "Rüzgar Hızı: " + ruzgarhizi;
+                labelGunDogumu.Text = "Gün Doğumu: " + gundogumu;
+                labelGunBatimi.Text = "Gün Batımı: " + gunbatimi;
+                labelSehir.Text = "Şehir: " + sehir;
+                labelNem.Text = "Nem Yüzdesi: " + nem;
+                labelBulutDurumu.Text = "Bulut Durumu: " + bulutdurumu;
+                labelBasınç.Text = "Basınç: " + basinc;
+            }
+            else if (kontrol==false)
+            {
+                labelSicaklik.Text = "";
+                labelHissedilenSicaklik.Text = "";
+                labelHavaDurumu.Text = "";
+                labelRuzgarHizi.Text = "";
+                labelGunDogumu.Text = "";
+                labelGunBatimi.Text = "";
+                labelSehir.Text = "";
+                labelNem.Text = "";
+                labelBulutDurumu.Text = "";
+                labelBasınç.Text = "";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelSehir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelNem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string sehirr = textBox1.Text;
+                havaDurumuAl(sehirr);
+            }
+        }
+        
+    }
+}
